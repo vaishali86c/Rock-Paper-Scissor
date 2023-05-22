16a73d2a5ded4a3c0cfafb5b86377dcbb296fd4e
vaishali86c <vaishali86c@gmail.com>
2023-05-22 19:58:58 +0530

    Update Project

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-
-
 function getComputerChoice() {
+
     let randomNumber = Math.floor(Math.random()*15);
+
     //console.log(randomNumber);
 
     if (randomNumber >= 0 && randomNumber <= 5){
@@ -14,16 +14,19 @@ function getComputerChoice() {
         return "scissor";
     }
 }
+
 //let choice = getComputerChoice();
 //console.log(choice);
-//playerselection
+
 function playerSelection() {
 
     let Rock;
     let Paper;
     let Scissor;
+
     playerinput = prompt("Please Enter rock,paper or scissor");
     let ans = playerinput.toLowerCase();
+
     return ans;
 
 }
@@ -40,29 +43,72 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == computerSelection) {
         return "Tie";
-    } else if (playerSelection == "rock") {
+
+    }else if (playerSelection == "rock") {
+
         if (computerSelection == "paper") {
             return "You lose ! Paper beats Rock";
         }else {
-            return "You win ! Rock beats Scissor";
+            return "You win  ! Rock beats Scissor";
         }
-    
+
     }else if (playerSelection == "paper") {
         if (computerSelection == "scissor") {
             return "You lose ! Scissors beats Paper";
-        } else {
-            return "You win ! Paper beats Rock";
+        }else {
+            return "You win  ! Paper beats Rock";
         }
-    } else if (playerSelection == "scissor") {
+
+    }else if (playerSelection == "scissor") {  
         if (computerSelection == "rock") {
             return "You lose ! Rock beats Scissor";
-        } else {
-            return "You win! Scissors beats paper";
+        }else {
+            return "You win  ! Scissors beats paper";
         }
+        
     }else {
         return "Select rock paper or scissor";
     }
 }
 
-//playerSelection();
-console.log(playRound(playerSelection(),getComputerChoice()));
+//directly function call
+//console.log(playRound(playerSelection(), getComputerChoice()));
+
+//5times run loop and declare winner
+
+function game() {
+
+    let computercount = 0;
+    let playercount = 0;
+    //1.call for loop 5 times
+   // let ans = playRound();
+
+    for (let i = 1;i <= 5;i++) {
+        let result = playRound(playerSelection(),getComputerChoice());
+        console.log(result);
+
+        if (result.slice(0,10) == "You lose !") {
+            computercount++;
+        }
+        if (result.slice(0,10) == "You win  !") {
+            playercount++;
+        }
+        if (result == "Tie"){
+            playercount++;
+            computercount++;
+        }
+        
+    }
+
+    if (playercount > computercount) {
+        console.log("player win with score " + playercount);
+
+    }else if(computercount > playercount) {
+        console.log("computer win with score " + computercount);
+    }else {
+        console.log("Tie with equal score ");
+    }
+}
+
+game();
+
